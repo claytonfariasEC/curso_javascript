8c0cbc837f9905d97fcab958146690a4e115b1e3
claytonfariasEC <47605415+claytonfariasEC@users.noreply.github.com>
2021-02-07 23:19:14 -0300

    First javascript project

---
+++ aula.js
@@ -0,0 +1,60 @@
+function setName(cliente, newname){   
+    cliente.nome = newname;  
+};
+
+console.log("Home world my bitche")
+
+let eusguri = 100000;
+
+let teste = 'Palmeiras não tem mundial'
+
+console.log(teste + " ~~~\n " + eusguri)
+
+let me = 232; 
+me = "teste"
+
+
+//Objeto 
+
+let cliente = {
+    nome: 'Claytin',
+    idade: 27,
+    cpf: 022111222333,
+}; 
+
+
+console.log(cliente)
+
+//array
+
+let array = [true, 45, "teste", 17];
+
+console.log(array)
+console.log(array.length)
+
+setName(cliente);
+
+
+for(let i=0; i < 10; i++){
+    setName(cliente, "BBB_"+i)
+    console.log("Novo Nome: " + cliente.nome )
+
+
+}
+const resultado = 12
+
+//Constructor Factory
+function criar_celular(marca, tela){
+    return{
+        marcaCelular: marca, 
+        tela: tela,
+
+        ligar(){
+          console.log("Fazendo ligação .....")
+        }
+    }
+}
+
+
+const celular = criar_celular("Iphone", "5.5")
+console.log(celular)
